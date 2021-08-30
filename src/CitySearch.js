a053a23acbaa88328e9519e5529341c178f720eb
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-08-30 15:00:37 -0500

    Add Alerts

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -13,12 +14,18 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-
-    return this.setState({
-      query: value,
-      suggestions,
-
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city.',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
@@ -26,7 +33,7 @@ class CitySearch extends Component {
       query: suggestion,
       suggestions: [],
       showSuggestions: false,
-
+      infoText: ''
     });
 
     this.props.updateEvents(suggestion, this.props.numberOfEvents);
@@ -35,6 +42,8 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+
+        <InfoAlert text={this.state.infoText} />
 
         <input
           type="text"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import { ErrorAlert } from './Alert';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   constructor(props) {
@@ -15,15 +15,23 @@ class NumberOfEvents extends Component {
     this.setState({
       numberOfEvents: value,
     });
-
-    this.props.changeCount(value);
+    if (value > 32) {
+      this.setState({
+        errorText: 'Please enter a number from 1 to 32.'
+      })
+    } else {
+      this.setState({
+        errorText: ''
+      })
+      this.props.changeCount(value);
+    }
   };
 
 
   render() {
     return (
       <div>
-
+        <ErrorAlert text={this.state.errorText} />
         <input
           type="number"
           className="number"
